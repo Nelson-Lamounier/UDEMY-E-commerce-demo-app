f6b067f1ff1828f504c1a267b31f7fd36c4c5b38
Nelson L <lamounierleao@gmail.com>
2024-05-26 15:20:39 +0100

    Checkout component and total cart added

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../context/cart.context";
 
 import Button from "../button/button.component";
@@ -10,6 +12,11 @@ import "./cart-dropdown.style.scss";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout')
+  }
 
   return (
     <div className="cart-dropdown-container">
@@ -22,7 +29,7 @@ const CartDropdown = () => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-        <Button>GO TO CHECKOUT</Button>
+        <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
